c30b00fd3cb7dfbbb2a2be21fa24ce199538fbe0
RenYufeng <seven__up@sina.cn>
2016-06-01 10:40:11 +0800

    update gradle config

---
+++ lib/src/main/java/cn/septenary/Hello.java
@@ -0,0 +1,12 @@
+package cn.septenary;
+
+import java.util.Arrays;
+
+/**
+ * Created by Septenary on 16/1/21.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello jar !" + Arrays.toString(args));
+    }
+}
